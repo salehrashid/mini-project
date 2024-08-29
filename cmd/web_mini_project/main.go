c9e91c34532de5e59d219b15a47bbf126504e5a7
salehrashid <saleh.rashid.b@gmail.com>
2024-08-29 21:53:01 +0700

    feat: add html form

--- cmd/web_mini_project/main.go
+++ cmd/web_mini_project/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/salehrashid/mini-project/internal/renderer"
 	"github.com/salehrashid/mini-project/internal/transport"
 	util "github.com/salehrashid/mini-project/internal/util"
@@ -11,6 +12,9 @@ func main() {
 	e := echo.New()
 	e.Renderer = renderer.NewTemplateRenderer(true)
 	e.Static("/assets", util.GetString("WEB_MINI_PROJECT_FILE_STATICS_PATH", "../../../web/static"))
+	
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 	
 	transport := transport.NewHTTPTransport()
 	transport.Router(e)
