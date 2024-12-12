e16624f6a447be6a5775d743953ea41cd3889ff1
salehrashid <saleh.rashid.b@gmail.com>
2024-12-12 18:35:46 +0700

    refactor: rename directory

--- internal/app/web_mini_project/renderer/renderer.go
+++ internal/app/web_mini_project/renderer/renderer.go
@@ -5,25 +5,24 @@ import (
 	"io"
 
 	"github.com/labstack/echo/v4"
-	"github.com/salehrashid/mini-project/internal/app/app_mini_project/util"
+	"github.com/salehrashid/mini-project/internal/app/web_mini_project/util"
 	model "github.com/salehrashid/mini-project/pkg/model"
-
 )
 
 // TemplateRenderer is a custom renderer for Echo that uses Go's html/template package.
 // It compiles templates and renders them with additional functions registered via funcMap.
 type TemplateRenderer struct {
-    // Template is the parsed template object containing all available HTML templates.
-    Template *template.Template
+	// Template is the parsed template object containing all available HTML templates.
+	Template *template.Template
 }
 
 // NewTemplateRenderer initializes a TemplateRenderer by parsing all templates in the directory.
 // It registers custom functions for use in templates via funcMap.
 // Panics if the templates cannot be parsed.
 func NewTemplateRenderer() *TemplateRenderer {
-    return &TemplateRenderer{
-        Template: template.Must(template.New("t").Funcs(funcMap()).ParseGlob(getTemplateDirectory())),
-    }
+	return &TemplateRenderer{
+		Template: template.Must(template.New("t").Funcs(funcMap()).ParseGlob(getTemplateDirectory())),
+	}
 }
 
 // Render writes the rendered template to the provided writer using the given data.
@@ -34,23 +33,23 @@ func NewTemplateRenderer() *TemplateRenderer {
 // - data: The data passed to the template for dynamic rendering.
 // - c: The Echo context (currently unused in this implementation).
 func (renderer *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-    return renderer.Template.ExecuteTemplate(w, name, data)
+	return renderer.Template.ExecuteTemplate(w, name, data)
 }
 
 // funcMap defines custom helper functions to be used in templates (HTML Files).
 // These functions can process data before rendering.
 // Example: getPageTitle extracts the title from a TemplateModel object.
 func funcMap() template.FuncMap {
-    return template.FuncMap{
-        "getPageTitle": func(templateData model.TemplateModel) string {
-            return templateData.Title
-        },
-    }
+	return template.FuncMap{
+		"getPageTitle": func(templateData model.TemplateModel) string {
+			return templateData.Title
+		},
+	}
 }
 
 // getTemplateDirectory retrieves the directory for template files.
 // The directory can be set via the environment variable WEB_MINI_PROJECT_TEMPLATE_PATH.
 // Defaults to "../../../web/template/html/*/*.html" if the variable is unset.
 func getTemplateDirectory() string {
-    return util.GetString("WEB_MINI_PROJECT_TEMPLATE_PATH", "../../../web/template/html/*/*.html")
+	return util.GetString("WEB_MINI_PROJECT_TEMPLATE_PATH", "../../../web/template/html/*/*.html")
 }
